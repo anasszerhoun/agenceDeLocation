077d135c67f02d00bf858615569762c263426ce5
ali harit <111861191+itsaliiiiiiii@users.noreply.github.com>
2024-12-10 14:41:11 +0100

    finish front | Ali | 10-12-24

--- frontend/car-rental/src/App.js
+++ frontend/car-rental/src/App.js
@@ -4,6 +4,10 @@ import './index.css';
 import Home from "./Components/Home/Home";
 import Login from "./Components/authentication/Login";
 import Register from "./Components/authentication/Register";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import CarPreview from "./Components/Home/CarPreview";
+import CheckoutPage from "./Components/Pay/CheckoutPage";
 
 function App() {
   return (
@@ -11,8 +15,10 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/carpreview" element={<CarPreview />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/pay" element={<CheckoutPage />} />
         </Routes>
       </div>
     </Router>
